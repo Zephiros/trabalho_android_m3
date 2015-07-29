5b698d558320af96c362438e9b3bbc3e82b2a2b3
Zephiros <zephiros@localhost.localdomain>
2015-07-29 04:48:47 -0300

    unlock

--- python/run.py
+++ python/run.py
@@ -6,6 +6,12 @@ app = Flask("__main__");
 def alive():
     return make_response("Alive", 200);
 
+@app.route("/unlock/<string:key>", methods=["GET"])
+def unlock(key):
+    if key == "senha":
+        return make_response("Unlocked", 200);
+    return make_response("Do you think that? It's so 'obivio'", 500);
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, threaded=True, debug=True);
 
